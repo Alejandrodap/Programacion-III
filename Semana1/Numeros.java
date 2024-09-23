0892b78e61156ffc096d53e6081c8719c30cdb3b
Alejandro D <alejandrodap@usal.es>
2024-09-23 18:33:43 +0200

    COrecion

---
+++ Semana1/Numeros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Numeros {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int suma = num1 + num2;
+
+        System.out.println("La suma de los dos números es: " + suma);
+    }
+}
